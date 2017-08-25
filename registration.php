899be5106d02c8d9f2cfa39bf9eed1e7423b511e
Piotr Kaminski <magentopeter@gmail.com>
2017-08-25 15:04:09 -0500

    Translation autoupdate from Crowdin for sk_SK - 2017-08-25 20:04:09 branch Head

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magentotwotranslations_sk_sk',
+    __DIR__
+);
